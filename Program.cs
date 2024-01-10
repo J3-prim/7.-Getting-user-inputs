93d9540ca3b1443622b3b6f8e1aa87e40239d4da
Justin Prim <justinprim140969@gmail.com>
2024-01-10 14:38:22 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Getting_user_inputs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Whats your name: ");
+            string name   = Console.ReadLine();
+            Console.WriteLine("Hi "+name);
+            Console.WriteLine("How old are you");
+            string age =  Console.ReadLine();
+            Console.WriteLine("You are " + age); 
+            Console.ReadLine();
+        }
+    }
+}
